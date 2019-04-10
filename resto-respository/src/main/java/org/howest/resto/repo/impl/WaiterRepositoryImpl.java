dc5da695a09d46295a460ad18c548f12dd967fa5
Kasper Cools <kasper.cools@howest.be>
2019-04-10 16:50:36 +0200

    v0.0.1

--- resto-respository/src/main/java/org/howest/resto/repo/impl/WaiterRepositoryImpl.java
+++ resto-respository/src/main/java/org/howest/resto/repo/impl/WaiterRepositoryImpl.java
@@ -5,6 +5,8 @@ import org.howest.resto.repo.UserRepository;
 import org.howest.resto.repo.WaiterRepository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class WaiterRepositoryImpl extends GenericRepositoryImpl<Integer, Waiter> implements WaiterRepository {
 
@@ -21,8 +23,16 @@ public class WaiterRepositoryImpl extends GenericRepositoryImpl<Integer, Waiter>
     }
 
     @Override
-    public List<Waiter> findAllByName(String name) {
+    public List<Waiter> findAllByFirstNameOrLastNameContaining(String query) {
 
-        return entityCollection.stream().filter(w->w.getLinkedUserAccount().getFirstName().contains(name))
+        return entityCollection.stream()
+                .filter(w -> w.getLinkedUserAccount().getFirstName().contains(query)
+                        || w.getLinkedUserAccount().getLastName().contains(query))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<Waiter> findByLogin(String login) {
+        return Optional.empty();
     }
 }
